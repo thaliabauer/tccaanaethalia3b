d7d9a09a5afc6b6957d8ae269b6733c4ec969d62
Thalia Bauer <thaliaquirinobauer.com>
2023-09-04 18:46:10 -0300

    Tapetes

--- sobre.php
+++ sobre.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="imagex/png" href="img/logotapetes.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
@@ -11,11 +13,19 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
-
-
+<title>Tapetes da Fabi</title>
 </head>
 <body>
-<h1>Seja bem vindo a loja virtual Tapetes da Fabi!</h1>
-<p>Tapetes da fabi está atuando desde 2020..</p>
+<div class="mt-3"></div>
+<h1 class="titulo">Seja bem vindo a loja virtual Tapetes da Fabi!</h1>
+<p class="paragrafo">Tapetes da fabi está atuando desde 2020 como costureira e artesã temos a Fabiana Dos Santos 
+Quirino Bauer criadora e proprietária desta micro-empresa e com a ajuda de sua equipe auxiliadora: Thalia Quirino Bauer
+e Thaís Quirino Bauer juntamente com Leonardo Homem De Azevedo</p>
+<h2 class="h2">Para qualquer dúvida ou algum interesse particular chame no nosso contato via whatsapp: (51)998566148 (Fabi) </h2>
+<h2 class="text-center">Confira também em nossas redes sociais <a href="https://instagram.com/tapetesdafabi?igshid=MzRlODBiNWFlZA==" target="blank"><span class="insta">Instagram</span></a> </h2>
+<div class="text-center img-fluid">
+<img src="img/qrcode.jpeg" alt="Qrcode Insta" style="width: 200px;">
+</div>
+
 </body>
 </html>
